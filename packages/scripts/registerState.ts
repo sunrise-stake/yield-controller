fbbcec80053daabbb7f7a600d34eec1ecf05e6bc
dankelleher <dankelleher@yahoo.com>
2023-01-18 06:17:50 +0100

    Lint fix

--- packages/scripts/registerState.ts
+++ packages/scripts/registerState.ts
@@ -1,55 +1,50 @@
-import { PROGRAM_ID, TreasuryControllerClient } from "../client";
+import { TreasuryControllerClient } from "../client";
 import * as anchor from "@project-serum/anchor";
-import { AnchorProvider } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
-import { getAssociatedTokenAddressSync ,TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { getAssociatedTokenAddressSync } from "@solana/spl-token";
 
 /** Adjust these values to whatever you want them to be */
 const PRICE = 1;
 const PURCHASE_THRESHOLD = 100;
 const PURCHASE_PROPORTION = 0;
 
-const defaultTreasuryKey  = "ALhQPLkXvbLKsH5Bm9TC3CTabKFSmnXFmzjqpTXYBPpu";
+const defaultTreasuryKey = "ALhQPLkXvbLKsH5Bm9TC3CTabKFSmnXFmzjqpTXYBPpu";
 const treasuryKey = new PublicKey(
-    process.env.TREASURY_KEY ?? defaultTreasuryKey
-  );
+  process.env.TREASURY_KEY ?? defaultTreasuryKey
+);
 
 const defaultAuthority = "5HnwQGT79JypiAdjdjsXEn1EMD2AsRVVubqDyWfyWXRv";
 const authorityKey = new PublicKey(
-    process.env.AUTHORITY_KEY ?? defaultAuthority
+  process.env.AUTHORITY_KEY ?? defaultAuthority
 );
 
 // used in devnet
 const defaultMint = "tnct1RC5jg94CJLpiTZc2A2d98MP1Civjh7o6ShmTP6";
-const mint = new PublicKey(
-    process.env.MINT ?? defaultMint
-);
+const mint = new PublicKey(process.env.MINT ?? defaultMint);
 
 const defaultHoldingAccount = "48V9nmW9awiR9BmihdGhUL3ZpYJ8MCgGeUoSWbtqjicv";
 const holdingAccount = new PublicKey(
-    process.env.HOLDING_ACCOUNT ?? defaultHoldingAccount
+  process.env.HOLDING_ACCOUNT ?? defaultHoldingAccount
 );
 
-(
-    async () => {
-        // get token account account for holding account
-        const holdingAccountTokenAddress = getAssociatedTokenAddressSync(
-            mint,
-            authorityKey,
-            true,
-        )
+(async () => {
+  // get token account account for holding account
+  const holdingAccountTokenAddress = getAssociatedTokenAddressSync(
+    mint,
+    authorityKey,
+    true
+  );
 
-        const client = await TreasuryControllerClient.register(
-            authorityKey,
-            treasuryKey,
-            mint,
-            holdingAccount,
-            holdingAccountTokenAddress,
-            new anchor.BN(PRICE),
-            PURCHASE_PROPORTION,
-            new anchor.BN(PURCHASE_THRESHOLD)
-        )
+  const client = await TreasuryControllerClient.register(
+    authorityKey,
+    treasuryKey,
+    mint,
+    holdingAccount,
+    holdingAccountTokenAddress,
+    new anchor.BN(PRICE),
+    PURCHASE_PROPORTION,
+    new anchor.BN(PURCHASE_THRESHOLD)
+  );
 
-        console.log("newly registerd state:", client.stateAddress);
-    }
-)().catch(console.error);
+  console.log("newly registerd state:", client.stateAddress);
+})().catch(console.error);
--- packages/scripts/util.ts
+++
@@ -1,3 +0,0 @@
-// get token account for holding account in registerState
-import { PublicKey } from "@solana/web3.js";
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
