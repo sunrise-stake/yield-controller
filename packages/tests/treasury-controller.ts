8f60e7acff9e8315bd4a3f6a9cae0e6178e957f3
k <mja>
2023-05-09 12:47:13 -0700

    upgrade to anchor 0.27.0, remove deprecated packages

--- packages/tests/treasury-controller.ts
+++ packages/tests/treasury-controller.ts
@@ -1,5 +1,5 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
 import { TreasuryController } from "../types/treasury_controller";
 import BN from "bn.js";
 import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
